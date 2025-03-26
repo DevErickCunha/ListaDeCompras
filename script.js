7a7a381b43fd9ea769e379dd61da81550a357d13
Erick Cunha <erickcuna20@gmail.com>
2025-03-25 21:47:27 -0300

    Criada função de click, exibindo modal com animação

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('a').addEventListener('click', (e) => {
+        e.preventDefault();
+        
+        d('.pizzaWindowArea').style.opacity = 0; // animação
+        d('.pizzaWindowArea').style.display = 'flex'; //substitui o display none por flex
+        setTimeout(() => {
+            d('.pizzaWindowArea').style.opacity = 1;  
+        }, 200);
+    });
 
     d('.pizza-area').append( pizzaItem);
 });
