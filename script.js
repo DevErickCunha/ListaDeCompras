b00c86b9138a6d9616f796cd979a7bd2e7ebb69b
Erick Cunha <erickcuna20@gmail.com>
2025-03-25 22:09:09 -0300

    Exibindo informações do item clicado no modal

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ const d = (el)=> document.querySelector(el);
 
 pizzaJson.map((item, index) => {
     let pizzaItem = d('.models .pizza-item').cloneNode(true);
-
+    pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
+
+        let key  = e.target.closest('.pizza-item').getAttribute('data-key');
+
+
+        d('.pizzaBig img').src = pizzaJson[key].img; //troca a imagem da pizza grande
+        d('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        d('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
         
         d('.pizzaWindowArea').style.opacity = 0; // animação
         d('.pizzaWindowArea').style.display = 'flex'; //substitui o display none por flex
@@ -20,3 +27,5 @@ pizzaJson.map((item, index) => {
 
     d('.pizza-area').append( pizzaItem);
 });
+
+
